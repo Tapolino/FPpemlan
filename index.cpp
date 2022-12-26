bd667d52078bd48d3e1bf548d4a01eb3dfd2b950
Bintang <121072490+hosheezora@users.noreply.github.com>
2022-12-26 16:00:01 +0700

    Data Fiks

--- index.cpp
+++ index.cpp
@@ -10,60 +10,71 @@ struct data_topskor
 	string klub;
 	int goal;
 	int asst;
+	double prob;
 };
 data_topskor tops[max];
 
 void datainput(struct data_topskor *tops)
 {
-	tops[0].klub = "Persib\t";
+	tops[0].klub = "Argentina";
 	tops[0].pemain = "Messi";
-	tops[0].goal = 15;
-	tops[0].asst = 26;
+	tops[0].goal = 17;
+	tops[0].asst = 8;
+	tops[0].prob;
 
-	tops[1].klub = "Persita ";
+	tops[1].klub = "Prancis ";
 	tops[1].pemain = "Mbappe";
-	tops[1].goal = 27;
-	tops[1].asst = 12;
+	tops[1].goal = 15;
+	tops[1].asst = 5;
+	tops[1].prob;
 
-	tops[2].klub = "Selangor FA";
-	tops[2].pemain = "Lautaro";
-	tops[2].goal = 12;
-	tops[2].asst = 8;
+	tops[2].klub = "Portugal ";
+	tops[2].pemain = "Ronaldo";
+	tops[2].goal = 18;
+	tops[2].asst = 6;
+	tops[2].prob;
 
-	tops[3].klub = "AS Roma ";
-	tops[3].pemain = "Boaz";
-	tops[3].goal = 19;
-	tops[3].asst = 14;
+	tops[3].klub = "Brazil ";
+	tops[3].pemain = "Neymar";
+	tops[3].goal = 15;
+	tops[3].asst = 9;
+	tops[3].prob;
 
-	tops[4].klub = "Persija ";
-	tops[4].pemain = "Ronaldi";
-	tops[4].goal = 14;
-	tops[4].asst = 5;
+	tops[4].klub = "England";
+	tops[4].pemain = "Harry Meguire";
+	tops[4].goal = 13;
+	tops[4].asst = 8;
+	tops[4].prob;
 
-	tops[5].klub = "Persipura";
-	tops[5].pemain = "Neymar";
-	tops[5].goal = 11;
-	tops[5].asst = 16;
+	tops[5].klub = "Jepang";
+	tops[5].pemain = "Maya Yoshida";
+	tops[5].goal = 12;
+	tops[5].asst = 8;
+	tops[5].prob;
 
-	tops[6].klub = "Martapura FC";
-	tops[6].pemain = "Halland";
-	tops[6].goal = 21;
-	tops[6].asst = 15;
+	tops[6].klub = "Polandia ";
+	tops[6].pemain = "Lewandowski";
+	tops[6].goal = 15;
+	tops[6].asst = 7;
+	tops[6].prob;
 
-	tops[7].klub = "Persis Solo";
-	tops[7].pemain = "Benzema";
-	tops[7].goal = 16;
+	tops[7].klub = "Kroasia";
+	tops[7].pemain = "Josko Gvardiol";
+	tops[7].goal = 13;
 	tops[7].asst = 7;
+	tops[7].prob;
 
-	tops[8].klub = "Persik Kediri";
-	tops[8].pemain = "Salah";
-	tops[8].goal = 18;
-	tops[8].asst = 13;
+	tops[8].klub = "Maroko";
+	tops[8].pemain = "Hakimi";
+	tops[8].goal = 10;
+	tops[8].asst = 5;
+	tops[8].prob;
 
-	tops[9].klub = "Persija ";
-	tops[9].pemain = "Nunez";
-	tops[9].goal = 26;
-	tops[9].asst = 10;
+	tops[9].klub = "Brazil";
+	tops[9].pemain = "Richarlison";
+	tops[9].goal = 11;
+	tops[9].asst = 5;
+	tops[9].prob;
 }
 
 void TopAssistWithShellSort(struct data_topskor *tops, int n)
@@ -94,99 +105,104 @@ void TopAssistWithShellSort(struct data_topskor *tops, int n)
 void TopScorerWithBubbleSort(struct data_topskor *tops, int n)
 {
 	int i, j;
-	for (i=0;i < n - 1;i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		for (j = 0; j < n - i - 1; j++)
 		{
-			if (tops[j].goal < tops[j+1].goal)
+			if (tops[j].goal < tops[j + 1].goal)
 			{
-			swap(tops[j].goal, tops[j + 1].goal);
-			swap(tops[j].pemain, tops[j +1].pemain);
-			swap(tops[j].asst, tops[j + 1].asst);
-			swap(tops[j].klub, tops[j + 1].klub);
-					
+				swap(tops[j].goal, tops[j + 1].goal);
+				swap(tops[j].pemain, tops[j + 1].pemain);
+				swap(tops[j].asst, tops[j + 1].asst);
+				swap(tops[j].klub, tops[j + 1].klub);
 			}
 		}
 	}
 }
 
 void Adddata(struct data_topskor *tops)
-{	
-			int size = 0;
-   				
-		for (int i = 0; i < max; i++) {
-		  if (tops[i].pemain.empty()) {
-		    break;
-		  }
-		  size++;
+{
+	int size = 0;
+
+	for (int i = 0; i < max; i++)
+	{
+		if (tops[i].pemain.empty())
+		{
+			break;
 		}
-		cout << "Jumlah pemain sekarang : " << size << endl;
-		
-			cout << " ==++Data Pemain Baru++==" << endl;
-			cout << " Klub: ";
-			cin>>tops[size].klub;
-		
-			cout << " Pemain: ";
-			cin>>tops[size].pemain;
-		
-			cout << " Goal: ";
-			cin >> tops[size].goal;
-			
-			cout << " Asst: ";
-			cin >> tops[size].asst;
-		
-			cout << endl;
-				
-			cout << tops[size].klub <<" dengan para pemain " << tops[size].pemain;
-			cout << " dengan goal yang tercetak " << tops[size].goal <<" memiliki asst " << tops[size].asst <<" dalam pertandingan " << endl;
+		size++;
+	}
+	cout << "Jumlah pemain sekarang : " << size << endl;
+
+	cout << " ==++Data Pemain Baru++==" << endl;
+	cout << " Klub: ";
+	cin >> tops[size].klub;
+
+	cout << " Pemain: ";
+	cin >> tops[size].pemain;
+
+	cout << " Goal: ";
+	cin >> tops[size].goal;
+
+	cout << " Asst: ";
+	cin >> tops[size].asst;
+
+	cout << endl;
+
+	cout << tops[size].klub << " dengan para pemain " << tops[size].pemain;
+	cout << " dengan goal yang tercetak " << tops[size].goal << " memiliki asst " << tops[size].asst << " dalam pertandingan " << endl;
 }
 
-void searchPemain(data_topskor tops[], int  n)
+void searchPemain(data_topskor tops[], int n)
 {
 	string nama;
-	int flag=0;
-			cout<<"  Cari Pemain : "; cin>>nama;
-			for(int i=0;i<n;i++)
-			{
-				if(tops[i].pemain == nama)
-				{
-					cout<<" ------------------------"<<endl; 
-					cout<<" Topscore ke "<<i+1<<endl;
-					cout<<" ------------------------"<<endl; 
-					cout<<" Nama     : "<<nama<<endl;
-					cout<<" Klub     : "<<tops[i].klub<<endl;
-					cout<<" Goal     : "<<tops[i].goal<<endl;
-					cout<<" Assist   : "<<tops[i].asst<<endl;
-					cout<<" ------------------------"<<endl;
-					flag++;
-					break;
-				}	
-			}
-			if(flag==0)
-			{
-				cout<< "\n  Nama pemain "<<nama<<" tidak ada dalam jajaran topscore\n"<<endl;
-			}
-			else
-			{
-				cout<<" "<<nama<<" ada dalam jajaran topscore"<<endl;
-			}
-			cout<<"\n\t\t\t TEKAN APAPUN UNTUK KEMBALI!\n";
-	getch();system("cls");
+	int flag = 0;
+	cout << "  Cari Pemain : ";
+	cin >> nama;
+	for (int i = 0; i < n; i++)
+	{
+		if (tops[i].pemain == nama)
+		{
+			cout << " ------------------------" << endl;
+			cout << " Topscore ke " << i + 1 << endl;
+			cout << " ------------------------" << endl;
+			cout << " Nama     : " << nama << endl;
+			cout << " Klub     : " << tops[i].klub << endl;
+			cout << " Goal     : " << tops[i].goal << endl;
+			cout << " Assist   : " << tops[i].asst << endl;
+			cout << " ------------------------" << endl;
+			flag++;
+			break;
+		}
+	}
+	if (flag == 0)
+	{
+		cout << "\n  Nama pemain " << nama << " tidak ada dalam jajaran topscore\n"
+				 << endl;
+	}
+	else
+	{
+		cout << " " << nama << " ada dalam jajaran topscore" << endl;
+	}
+	cout << "\n\t\t\t TEKAN APAPUN UNTUK KEMBALI!\n";
+	getch();
+	system("cls");
 }
 
-void display(data_topskor *tops,int n)
+void display(data_topskor *tops, int n)
 {
-	
-	cout<<" +------+------------------------+---------------+---------------+---------------+\n";
-	cout<<" | No\t|\tKlub\t\t | \tPemain\t | \tGoal\t | \tAssist\t |\n";
-	cout<<" +------+------------------------+---------------+---------------+---------------+\n";
-	for(int i=0;i<n;i++)
+
+	cout << " +------+------------------------+---------------+---------------+---------------+\n";
+	cout << " | No\t|\tKlub\t\t | \tPemain\t | \tGoal\t | \tAssist\t |\n";
+	cout << " +------+------------------------+---------------+---------------+---------------+\n";
+	for (int i = 0; i < n; i++)
 	{
-		cout<<" | "<<i+1<<"\t|\t"<<tops[i].klub<<"\t | \t"<<tops[i].pemain<<"\t | \t"<<tops[i].goal<<"\t | \t"<<tops[i].asst<<"\t |\n";
+		cout << " | " << i + 1 << "\t|\t" << tops[i].klub << "\t | \t" << tops[i].pemain << "\t | \t" << tops[i].goal << "\t | \t" << tops[i].asst << "\t |\n";
 	}
-	cout<<" +------+------------------------+---------------+---------------+---------------+\n";
-	cout<<"\n\t\t\t   TEKAN APAPUN UNTUK KEMBALI!\n";
-	getch(); system("cls");
+	cout << " +------+------------------------+---------------+---------------+---------------+\n";
+	cout << "\n\t\t\t   TEKAN APAPUN UNTUK KEMBALI!\n";
+	getch();
+	system("cls");
 }
 
 int main()
@@ -197,61 +213,62 @@ int main()
 	data_topskor tops[max];
 	datainput(tops);
 
-	//display(tops,max);
+	// display(tops,max);
 	do
 	{
-		
-		cout<<" ========================\n";
-		cout<<" |         MENU         |\n";
-		cout<<" +----------------------+\n";
-		cout<<" | 1. Display Player\t|\n | 2. Top 5 Goalscorer\t|\n | 3. Top 5 Assister\t|\n | 4. Add Data\t|\n | 5. Searching\t\t|\n | 6. Exit\t\t|\n";
-		cout<<" ========================\n";
-		cout<<"  Masukkan pilihan -> "; cin>>menu;
+
+		cout << " ========================\n";
+		cout << " |         MENU         |\n";
+		cout << " +----------------------+\n";
+		cout << " | 1. Display Player\t|\n | 2. Top 5 Goalscorer\t|\n | 3. Top 5 Assister\t|\n | 4. Add Data\t|\n | 5. Searching\t\t|\n | 6. Exit\t\t|\n";
+		cout << " ========================\n";
+		cout << "  Masukkan pilihan -> ";
+		cin >> menu;
 		printf("\n");
 		switch (menu)
 		{
 		case 1:
 		{
 			// DISPLAY SELURUH DATA TANPA SORTING
-			cout<<"\t\t\t\tGOALSCORER\n";
+			cout << "\t\t\t\tGOALSCORER\n";
 			datainput(tops);
-			display(tops,max);
+			display(tops, max);
 
 			break;
 		}
 		case 2:
 		{
-			//display top goal max 5 data teratas
-			cout<<"\t\t\t\tDATA TOPSCORE\n";
+			// display top goal max 5 data teratas
+			cout << "\t\t\t\tDATA TOPSCORE\n";
 			TopScorerWithBubbleSort(tops, length);
-			display(tops,5);
+			display(tops, 5);
 			break;
 		}
 		case 3:
 		{
-			cout<<"\t\t\t\tDATA TOP ASSIST\n";
+			cout << "\t\t\t\tDATA TOP ASSIST\n";
 			TopAssistWithShellSort(tops, length);
-			display(tops,5);
+			display(tops, 5);
 			break;
 		}
 		case 4:
 		{
-			//menambahkan menu data
-			cout<<"\t\t\t\tAdd data\n";
+			// menambahkan menu data
+			cout << "\t\t\t\tAdd data\n";
 			Adddata(tops);
-			display(tops,max);
+			display(tops, max);
 			break;
 		}
 		case 5:
 		{
-			//search
-			searchPemain(tops,max);
+			// search
+			searchPemain(tops, max);
 			break;
 		}
 		case 6:
-			{
-				cout<<" \n\t\t\tKELUAR PROGRAM\n\n";
-			}
+		{
+			cout << " \n\t\t\tKELUAR PROGRAM\n\n";
+		}
 		default:
 		{
 			system("cls");
